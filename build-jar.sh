79ea0c99f98c24c1fdc295eecb3ea4ca104a82fa
KeaCluster <zzabdiel29@gmail.com>
2024-03-04 16:43:19 -0600

    Confirmations and ordeals

--- build-jar.sh
+++ build-jar.sh
@@ -8,22 +8,35 @@ echo "Make sure your API is cloned inside a certain *-api directory inside your 
 echo "$TABS"
 
 # Attempt to automatically find the application directory
-APP_DIR=$(find ~/ -type d -name "*-api" 2>/dev/null)
+APP_DIRS=($(find ~/ -type d -name "*-api" 2>/dev/null))
 
 # Check if the APP_DIR variable is empty and prompt for manual input if necessary
-[ -z "$APP_DIR" ] &&
+[ -z "${APP_DIRS[0]}" ] &&
 	echo "App directory not found." &&
 	echo "Please enter the path to your application's directory:" &&
-	read APP_DIR
+	read APP_DIR ||
+	APP_DIR="${APP_DIRS[0]}" # Default to the first found dir_
 
 echo "$TABS"
 echo "Building the .jar ..."
 
-sudo chmod 700 "$APP_DIR/gradlew"
-
-"$APP_DIR/gradlew" build -x test
+sudo chmod +x "$APP_DIR/gradlew" &&
+	"$APP_DIR/gradlew" build -x test &&
+	echo "Build sucessful." ||
+	{
+		echo "Build failed. Exiting."
+		exit1
+	}
 
 echo "$TABS"
-echo "Now copying the .jar to ~/app/"
+echo "Now copying the .jar into ~/app/"
 
-cp "$APP_DIR/build/libs/*.jar" ~/app/app.jar
+# Find the first .jar file
+JAR_FILE=$(find "$APP_DIR/build/libs" -name "*.jar" ! -name "*-plain.jar" -print -quit)
+
+[ -f "$JAR_FILE"] &&
+	cp "$JAR_FILE" ~/app/app.jar ||
+	{
+		echo "No .jar file was found inside build/libs. \nExiting."
+		exit 1
+	}
