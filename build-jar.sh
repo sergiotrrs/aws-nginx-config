8df5ac768fafc3dbb2416cc992b2984943136c48
KeaCluster <zzabdiel29@gmail.com>
2024-03-20 17:19:49 -0600

    Functional

--- build-jar.sh
+++ build-jar.sh
@@ -13,14 +13,53 @@ APP_DIRS=($(find ~/ -type d -name "*-api" 2>/dev/null))
 determine_build_tool_dir() {
 	local dir=$1
 
+	[[ -f "$dir/pom.xml" ]] && echo "maven:$dir" && return
+	[[ -f "$dir/build.gradle" || -f "$dir/build.gradle.kts" ]] &&
+		echo "gradle:$dir" && return
+	echo "none"
 }
 
 build_jar() {
 	local tool_dir=($1)
+	local tool=${tool_dir[0]}
+	local dir=${tool_dir[1]}
+
+	case $tool in
+	maven)
+		mvn -f "$dir/pom.xml" clean package -DskipTests >/dev/null 2>&1
+		;;
+	gradle)
+		sudo chmod +x "$dir/gradlew"
+		"$dir/gradlew" -p "$dir" build -x test >/dev/null 2>&1
+		;;
+	*)
+		return 1
+		;;
+	esac
 }
 
 cp_jar() {
 	local tool_dir=($1)
+	local tool=${tool_dir[0]}
+	local dir=${tool_dir[1]}
+	local jar_path=""
+
+	case $tool in
+	maven)
+		jar_path=$(find "$dir/target" -name "*.jar" ! -name "*-plain.jar" ! -name "*-javadoc.jar" ! -name "*-sources.jar" ! -name "*-javadoc.jar" -print -quit)
+		;;
+	gradle)
+		jar_path=$(find "$dir/build/libs" -name "*.jar" ! -name "*-plain.jar" -print -quit)
+		;;
+	esac
+
+	[[ -z "$jar_path" ]] &&
+		{
+			echo "No .jar file was found"
+			exit 1
+		}
+	cp "$jar_path" ~/app/app.jar &&
+		echo "Jar copied succesfully into ~/app/"
 }
 
 # Check if the APP_DIR variable is empty and prompt for manual input if necessary
